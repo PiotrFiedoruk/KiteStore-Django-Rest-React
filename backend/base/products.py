1217bd4a53d56eb9a1f8cd22d2d3a7b6e9cdc61a
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2021-05-12 13:33:59 +0200

    add backend api

---
+++ backend/base/products.py
@@ -0,0 +1,27 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Cabrinha Moto 2020',
+        'image': '/images/moto2020.jpg',
+        'description': 'Versatile kite for beginners and advanced riders',
+        'brand': 'Cabrinha',
+        'category': 'Kites',
+        'price': 299.99,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12,
+    },
+    {
+        '_id': '2',
+        'name': 'Duotone Rebel 2020',
+        'image': '/images/rebel2021.png',
+        'description': 'Versatile kite for beginners and advanced riders',
+        'brand': 'Duotone',
+        'category': 'Kites',
+        'price': 289.99,
+        'countInStock': 0,
+        'rating': 4.0,
+        'numReviews': 2,
+    },
+
+]
