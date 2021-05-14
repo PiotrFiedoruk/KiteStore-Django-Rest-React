a85031e50a6d424e7d6290049cd6a9884fc1e069
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2021-05-15 00:09:50 +0200

    add product page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
     return (
         <header>
             <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-              <Navbar.Brand href="/">
-                  <img alt="" src="../public/StoreLogo192.png" width="30" height="30"
-                       className="d-inline-block align-top"/>{' '}
-                  Kite Store
-                </Navbar.Brand>
+                  <LinkContainer to='/'>
+                      <Navbar.Brand>
+                           <img alt="" src="../public/StoreLogo192.png" width="30" height="30"
+                           className="d-inline-block align-top"/>{' '}
+                                Kite Store
+                      </Navbar.Brand>
+                  </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                  <Nav.Link href="/basket"><i className="fas fa-shopping-cart"></i> Basket</Nav.Link>
-                  <Nav.Link href="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+                    <LinkContainer to="basket">
+                        <Nav.Link><i className="fas fa-shopping-cart"></i> Basket</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='login'>
+                        <Nav.Link><i className="fas fa-user"></i> Login</Nav.Link>
+                    </LinkContainer>
                 </Nav>
               </Navbar.Collapse>
                 </Container>
