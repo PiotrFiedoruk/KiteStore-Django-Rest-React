a55dcc12fa93a2c18a3cead72bb4e51e6bc6a58a
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2021-05-14 23:00:18 +0200

    add routers

---
+++ frontend/src/products.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        '_id': '1',
+        'name': 'Cabrinha Switchblade 2020',
+        'image': '/images/cabrinhaSwitchBlade2020.jpg',
+        'description': 'some description here',
+        'brand': 'Cabrinha',
+        'category': 'Kites',
+        'price': 89.99,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 3,
+    },
+    {
+        '_id': '2',
+        'name': 'Cabrinha Moto 2020',
+        'image': '/images/cabtrinhamoto.jpg',
+        'description': 'some description here',
+        'brand': 'Cabrinha',
+        'category': 'Kites',
+        'price': 299.99,
+        'countInStock': 0,
+        'rating': 3,
+        'numReviews': 3,
+    }
+]
+export default products
